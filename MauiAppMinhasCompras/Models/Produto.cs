b8103deae4474a08c2425d9e5c9ee46aa1660ff3
Reginaldo <acordeseller@gmail.com>
2026-08-02 20:15:45 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    internal class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+    }
+}
